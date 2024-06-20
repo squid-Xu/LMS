44cd9f1784e30b48afbe9a802866530d3c3bdd4e
徐梦强 <1135349080@qq.com>
2024-06-20 14:48:57 +0800

    封装框架

---
+++ egg-api/app/router.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Egg.Application} app - egg application
+ */
+module.exports = app => {
+	const { router, controller } = app;
+
+	// 路由前缀
+	router.prefix('/lsm-api/v1');
+
+	// router.get('/', controller.home.index);
+	
+	//管理员
+	router.post('/admin/login', controller.admin.login);
+	router.get('/admin/info', controller.admin.info);
+};
---
+++ egg-api/config/config.local.js
@@ -0,0 +1,28 @@
+module.exports = () => {
+
+  const config = exports = {};
+
+  config.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: 'mysql.sqlpub.com',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root_dev',
+      // 密码
+      password: 'logLPS2KoBQ637us',
+      // 数据库名
+      database: 'lms_api',
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  };
+
+  return {
+    ...config,
+  };
+};
