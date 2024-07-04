2125e19c18c8c7ef1843626c8a378d27c31397e2
徐梦强 <1135349080@qq.com>
2024-07-04 09:56:44 +0800

    数据库配置

--- egg-api/config/config.prod.js
+++ egg-api/config/config.prod.js
@@ -6,13 +6,13 @@ module.exports = () => {
     // 单数据库信息配置
     client: {
       // host
-      host: 'mysql.sqlpub.com',
+      host: '47.120.33.36',
       // 端口号
       port: '3306',
       // 用户名
-      user: 'root_dev',
+      user: 'root_prod',
       // 密码
-      password: 'logLPS2KoBQ637us',
+      password: 'dkTZJtksDdTTtHZK',
       // 数据库名
       database: 'lms_api',
       timezone: '08:00',
